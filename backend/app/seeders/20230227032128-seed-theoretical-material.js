87b53d78e90f0d010046d69c3680f3244ff13744
José Enrique <drilosv13@gmail.com>
2023-03-19 20:28:04 -0600

    feat:Se añade material teorico de palabras atonas

--- backend/app/seeders/20230227032128-seed-theoretical-material.js
+++ backend/app/seeders/20230227032128-seed-theoretical-material.js
@@ -384,6 +384,62 @@ module.exports = {
       ],
       {},
     )
+    // Átonas
+    await TheoreticalMaterial.create({
+      name: 'Palabras tónicas',
+      theme_id: 6,
+      source:
+        '<p class="text-center text-h3 col-12 q-py-lg"> Palabras átonas </p>' +
+        '<p class="text-justify q-px-md">' +
+        'Son palabras átonas todas <b>aquellas que se pronuncian sin acento en la cadena hablada.</b> La gran mayoría de las palabras monosílabas, y unas cuantas bisílabas y trisílabas son átonas. ' +
+        'La mayor parte de las palabras trisílabas son naturalmente tónicas. Así, pues, en español son átonas las siguientes clases de palabras: ' +
+        '</p>' +
+        '<ul>' +
+        '<li class="q-my-sm"><b>Artículos</b>: el, la, lo, los, las. Por ejemplo: <i>el</i> cielo, <i>la</i> lluvia, <i>los</i> días.</li>' +
+        '<li class="q-my-sm"><b>Posesivos</b>: cuando están antepuestos al nombre: mi, tu, su, nuestro, nuestro, y sus formas femeninas y plurales. Por ejemplo: <i>mi</i> papá, <i>tu</i> traje, <i>nuestro</i> uniforme.</li>' +
+        '<li class="q-my-sm">Algunos <b>adverbios</b> como <i>tan</i>, <i>medio</i>. Por ejemplo: <i>tan</i> desilusionado, <i>medio</i> loco. También aun en ciertos casos: <i>aun</i> los más críticos lo aceptaron.</li>' +
+        '<li class="q-my-sm">Algunos <b>sustantivos</b> cuando son utilizados como fórmula de tratamiento y se encuentran antepuestos a nombres propios: don, doña, fray, sor, san, santo, santa. Por ejemplo: <i>doña</i> Consuelo, <i>santo</i> Tomás, <i>sor</i> Juana.</li>' +
+        '<li class="q-my-sm"><b>Pronombres personales:</b> me, te, se, lo, los, la, las, le, les, nos. Por ejemplo: <i>me</i> vio, <i>se</i> cayó, <i>la</i> busqué, <i>nos</i> encontró.</li>' +
+        '<li class="q-my-sm"><b>Relativos:</b> que, quien, donde, como, cuando, cuanto, salvo cual. Por ejemplo: la universidad <i>donde</i> estudié, el modo como me habla, hice <i>cuanto</i> pude.</li>' +
+        '<li class="q-my-sm"><b>Conjunciones</b>, salvo algunas excepciones: uno y otro, lo haremos <i>aunque</i> se oponga, no lo sé, <i>pues</i> no me avisó, etc.</li>' +
+        '<li class="q-my-sm"><b>Preposiciones</b>, excepto según: <i>para</i> dentro, <i>en</i> casa, <i>por</i> tonto, <i>con</i> dinero, <i>sin</i> amigos.</li>' +
+        '</ul>' +
+        '<br/>' +
+        '<p class="text-center text-h5 col-12 q-py-lg"> Palabras tónicas que se pronuncian como átonas </p>' +
+        '<p class="text-justify q-px-md"> Algunas palabras tónicas se pronuncian como átonas en determinados casos, principalmente cuando son el primer elemento en ciertas expresiones ' +
+        'que se encuentran formadas por varias palabras escritas separadamente. Así, pues, aunque son tónicas, son pronunciadas como átonas en los siguientes casos: <p>' +
+        '<ul>' +
+        '<li class="q-my-sm">Cuando constituyen el primer elemento de numerales complejos: cuarenta mil (cuarenta<b>míl</b>), treinta y dos (treitai<b>dós</b>), vigésimo primero (vigesimo<b>priméro</b>).</li>' +
+        '<li class="q-my-sm">Cuando son el primer elemento de una locución: cuesta abajo (cuest<b>abájo</b>), medio ambiente (medio<b>ambiénte</b>), mientras tanto (mientras<b>tánto</b>).</li>' +
+        '<li class="q-my-sm">Cuando se antepone un sustantivo o adjetivo a otro sustantivo, siendo que la expresión nominal funciona como vocativo: capitán Ochoa (capitan<b>ochóa</b>), buen hombre (buen<b>ómbre</b>).</li>' +
+        '</ul>' +
+        '<br/>' +
+        '<p class="text-justify q-px-md">' +
+        'En la cadena hablada, las palabras átonas, puesto que no tienen acento propio, se comportan de una manera particular: ' +
+        'se “apoyan” en la sílaba tónica de la palabra que las sigue o antecede, con la cual formarán lo que denominamos como grupo acentual.' +
+        '</p>' +
+        '<p class="text-justify q-px-md"> Dependiendo de la posición de la palabra con la que formen el grupo acentual, las palabras átonas podrán ser de dos tipos: <b>proclíticas</b> y <b>enclíticas</b>: </p>' +
+        '<p class="text-justify q-px-md">' +
+        '<b>Proclíticas</b> son aquellas palabras átonas que <b>forman grupo acentual con la palabra tónica que tienen a continuación.</b>' +
+        'Así, por ejemplo, en el enunciado: se lo esperaba, las palabras proclíticas serán <i>se</i> y <i>lo</i>, que se apoyan en la sílaba tónica del verbo: seloesperába.' +
+        '</p>' +
+        '<p class="text-justify q-px-md">' +
+        '<b>Enclíticas</b> son aquellas palabras átonas que <b>forman grupo acentual con la palabra tónica que las antecede.</b> ' +
+        'Por ejemplo, en el enunciado: piénsatelo, las palabras enclíticas son los pronombres te y lo, que se pronuncian apoyados en el verbo.' +
+        '</p>',
+    })
+    await queryInterface.bulkInsert(
+      'SourceOrigin',
+      [
+        {
+          theoretical_material_id: 6,
+          updated_at: '2023-03-19',
+          source_origin: 'https://www.diccionariodedudas.com/palabras-tonicas-y-atonas/',
+        },
+      ],
+      {},
+    )
+    // Tritónicas
   },
 
   async down(queryInterface, Sequelize) {
--- frontend/src/pages/ResponsePage.vue
+++ frontend/src/pages/ResponsePage.vue
@@ -35,26 +35,25 @@ export default defineComponent({
      */
     const title = ref('')
     const parrafo1 = ref(
-      '<p class="text-center text-h3 col-12 q-py-lg"> Palabras tónicas </p>' +
-      '<p class="text-justify q-px-md">Las palabras tónicas <b>son todas aquellas palabras que se pronuncian acentuando una de sus sílabas</b>. ' +
-        'La sílaba acentuada de la palabra tónica, a su vez, se denominará sílaba tónica. ' +
-        'No todas las palabras en español se perciben, en la lengua hablada, con el mismo grado de tonicidad. ' +
-        'Son tónicas las siguientes clases de palabras. </p>' +
-'<ul>' +
-'<li class="q-my-sm"><b>Sustantivos</b> la <i>puerta</i>, la <i>pelota</i>, el <i>amigo</i>, el <i>jardín</i>.</li>' +
-'<li class="q-my-sm"><b>Adjetivos</b> la <i>bonita</i> puerta, el jardín <i>bello</i>.</li>' +
-'<li class="q-my-sm"><b>Verbos y sus formas auxiliares</b> <i>hice</i> la comida, <i>ha</i> llovido, <i>ha</i> entrado.</li>' +
-'<li class="q-my-sm"><b>Adverbios</b> <i>no</i> lloverá, estuvo <i>bien</i>, <i>nunca</i> viene. También los adverbios terminados en <i>-mente</i></b> vienen <i>lentamente</i>, celebran <i>alegremente</i>.</li>' +
-'<li class="q-my-sm"><b>Pronombres personales</b> yo, tú, él, ella, ellos, ellas, nosotros, nosotras, vosotros, vosotras, nos, vos, usted, ustedes, mí, ti, sí, conmigo, contigo, consigo.</li>' +
-'<li class="q-my-sm"><b>Demostrativos</b> <i>esa</i> lámpara, <i>este</i> carro, <i>aquella</i> puerta.</li>' +
-'<li class="q-my-sm"><b>Posesivos</b> mío, tuyo, suyo, nuestro, vuestro, y sus femeninos y plurales.</li>' +
-'<li class="q-my-sm"><b>Indefinidos</b> <i>unos</i> chicos, <i>ninguna</i> canción, <i>algún</i> pariente, <i>varias</i> personas.</li>' +
-'<li class="q-my-sm"><b>Numerales</b> <i>dos</i> sillas, en <i>tercer puesto</i>, <i>quinientas</i> veces.</li>' +
-'<li class="q-my-sm"><b>Interrogativos y exclamativos</b> <i>qué</i> quieres, <i>cuántas</i> personas, <i>quién</i> es, dime <i>cuál</i>.</li>' +
-'<li class="q-my-sm">Algunas <b>conjunciones</b>, sobre todo si son derivadas de adverbios como, por ejemplo, <i>así</i>: “no lo reconoce, <i>así</i> se lo digas mil veces”; o <i>apenas</i>: “apenas me vio, me identificó”. También son tónicos algunos usos de la conjunción y, como, por ejemplo: “¿<i>y</i> viene?”, “¿<i>y</i> el vaso de agua?”</li>' +
-'<li class="q-my-sm"><b>Preposición según.</b> Por ejemplo: “Todo es positivo <i>según</i> se vea”.</li>' +
+      '<p class="text-center text-h3 col-12 q-py-lg"> Palabras átonas </p>' +
+      '<p class="text-justify q-px-md">' +
+        'Son palabras átonas todas <b>aquellas que se pronuncian sin acento en la cadena hablada.</b> La gran mayoría de las palabras monosílabas, y unas cuantas bisílabas y trisílabas son átonas. ' +
+        'La mayor parte de las palabras trisílabas son naturalmente tónicas. Así, pues, en español son átonas las siguientes clases de palabras: ' +
+      '</p>' +
+
+      '<ul>' +
+      '<li class="q-my-sm"><b>Artículos</b>: el, la, lo, los, las. Por ejemplo: <i>el</i> cielo, <i>la</i> lluvia, <i>los</i> días.</li>' +
+      '<li class="q-my-sm"><b>Posesivos</b>: cuando están antepuestos al nombre: mi, tu, su, nuestro, nuestro, y sus formas femeninas y plurales. Por ejemplo: <i>mi</i> papá, <i>tu</i> traje, <i>nuestro</i> uniforme.</li>' +
+      '<li class="q-my-sm">Algunos <b>adverbios</b> como <i>tan</i>, <i>medio</i>. Por ejemplo: <i>tan</i> desilusionado, <i>medio</i> loco. También aun en ciertos casos: <i>aun</i> los más críticos lo aceptaron.</li>' +
+      '<li class="q-my-sm">Algunos <b>sustantivos</b> cuando son utilizados como fórmula de tratamiento y se encuentran antepuestos a nombres propios: don, doña, fray, sor, san, santo, santa. Por ejemplo: <i>doña</i> Consuelo, <i>santo</i> Tomás, <i>sor</i> Juana.</li>' +
+      '<li class="q-my-sm"><b>Pronombres personales:</b> me, te, se, lo, los, la, las, le, les, nos. Por ejemplo: <i>me</i> vio, <i>se</i> cayó, <i>la</i> busqué, <i>nos</i> encontró.</li>' +
+      '<li class="q-my-sm"><b>Relativos:</b> que, quien, donde, como, cuando, cuanto, salvo cual. Por ejemplo: la universidad <i>donde</i> estudié, el modo como me habla, hice <i>cuanto</i> pude.</li>' +
+      '<li class="q-my-sm"><b>Conjunciones</b>, salvo algunas excepciones: uno y otro, lo haremos <i>aunque</i> se oponga, no lo sé, <i>pues</i> no me avisó, etc.</li>' +
+      '<li class="q-my-sm"><b>Preposiciones</b>, excepto según: <i>para</i> dentro, <i>en</i> casa, <i>por</i> tonto, <i>con</i> dinero, <i>sin</i> amigos.</li>' +
+
 '</ul>' +
 '<br/>' +
+
 '<p class="text-center text-h5 col-12 q-py-lg"> Palabras tónicas que se pronuncian como átonas </p>' +
 '<p class="text-justify q-px-md"> Algunas palabras tónicas se pronuncian como átonas en determinados casos, principalmente cuando son el primer elemento en ciertas expresiones ' +
 'que se encuentran formadas por varias palabras escritas separadamente. Así, pues, aunque son tónicas, son pronunciadas como átonas en los siguientes casos: <p>' +
@@ -62,7 +61,21 @@ export default defineComponent({
     '<li class="q-my-sm">Cuando constituyen el primer elemento de numerales complejos: cuarenta mil (cuarenta<b>míl</b>), treinta y dos (treitai<b>dós</b>), vigésimo primero (vigesimo<b>priméro</b>).</li>' +
 '<li class="q-my-sm">Cuando son el primer elemento de una locución: cuesta abajo (cuest<b>abájo</b>), medio ambiente (medio<b>ambiénte</b>), mientras tanto (mientras<b>tánto</b>).</li>' +
 '<li class="q-my-sm">Cuando se antepone un sustantivo o adjetivo a otro sustantivo, siendo que la expresión nominal funciona como vocativo: capitán Ochoa (capitan<b>ochóa</b>), buen hombre (buen<b>ómbre</b>).</li>' +
-  '</ul>'
+'</ul>' +
+  '<br/>' +
+  '<p class="text-justify q-px-md">' +
+        'En la cadena hablada, las palabras átonas, puesto que no tienen acento propio, se comportan de una manera particular: ' +
+        'se “apoyan” en la sílaba tónica de la palabra que las sigue o antecede, con la cual formarán lo que denominamos como grupo acentual.' +
+      '</p>' +
+  '<p class="text-justify q-px-md"> Dependiendo de la posición de la palabra con la que formen el grupo acentual, las palabras átonas podrán ser de dos tipos: <b>proclíticas</b> y <b>enclíticas</b>: </p>' +
+  '<p class="text-justify q-px-md">' +
+    '<b>Proclíticas</b> son aquellas palabras átonas que <b>forman grupo acentual con la palabra tónica que tienen a continuación.</b>' +
+    'Así, por ejemplo, en el enunciado: se lo esperaba, las palabras proclíticas serán <i>se</i> y <i>lo</i>, que se apoyan en la sílaba tónica del verbo: seloesperába.' +
+    '</p>' +
+  '<p class="text-justify q-px-md">' +
+    '<b>Enclíticas</b> son aquellas palabras átonas que <b>forman grupo acentual con la palabra tónica que las antecede.</b> ' +
+    'Por ejemplo, en el enunciado: piénsatelo, las palabras enclíticas son los pronombres te y lo, que se pronuncian apoyados en el verbo.' +
+    '</p>'
 
     )
     onMounted(() => {
