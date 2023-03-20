bcb96668d0ad9f1434aa1a54340f53ab4890deff
José Enrique <drilosv13@gmail.com>
2023-03-19 20:53:02 -0600

    feat:Se añade material teorico de palabras tritonicas

--- backend/app/seeders/20230227032128-seed-theoretical-material.js
+++ backend/app/seeders/20230227032128-seed-theoretical-material.js
@@ -440,6 +440,68 @@ module.exports = {
       {},
     )
     // Tritónicas
+    await TheoreticalMaterial.create({
+      name: 'Palabras tritónicas',
+      theme_id: 7,
+      source:
+        '<p class="text-center text-h3 col-12 q-py-lg"> Palabras tritónicas </p>' +
+        '<p class="text-justify q-px-md">' +
+        'Hay palabras que tienen los mismos sonidos y que se escriben con las mismas letras que otras, pero la situación de su sílaba tónica varía y con ella también cambia el significado. ' +
+        'Se llaman tritónicas aquellas palabras que pueden tener la sílaba tónica en tres (tri-) posiciones diferentes, su acento es lo que las diferencia.' +
+        '</p>' +
+        '<p class="text-justify q-px-md">Veamos el siguiente ejemplo:</p>' +
+        '<p class="text-justify q-px-md">Ejemplo 1: Término / Termino / Terminó  </p>' +
+        '<ul>' +
+        '<li class="q-my-sm">Por favor, no me hables en esos <b><i>términos</i></b></li>' +
+        '<li class="q-my-sm">Ya casi <i><b>termino</i></b> el documento y cuando lo tenga listo, te lo envío</li>' +
+        '<li class="q-my-sm">Andrés <b><i>terminó</i></b> la tarea muy rápido y después salió a cine con su novia</li>' +
+        '</ul>' +
+        '<br/>' +
+        '<p class="text-justify q-px-md">Este ejercicio es muy importante porque cuando estamos aprendiendo el modo subjuntivo del español, sucede con mucha frecuencia que se confunde la pronunciación del verbo en subjuntivo con la pronunciación del verbo en pasado.</p>' +
+        '<p class="text-justify q-px-md">Es muy diferente decir <i>“Yo hablé con ella ayer”</i> a decir <i>“Ojalá, ella hable mañana en la reunión”</i>.</p>' +
+        '<p class="text-center col-12 q-py-lg"> Ejemplos de palabras tritónicas </p>' +
+        '<table style="border-collapse: collapse; width: 100%;">' +
+        '<tbody>' +
+        '<tr>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Cálculo</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Calculo</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Calculó</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Práctico</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Practico</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Practicó</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Público</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Publico</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Publicó</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Género</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Genero</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Genero</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Título</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Titulo</td>' +
+        '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Tituló</td>' +
+        '</tr>' +
+        '</tbody>' +
+        '</table>' +
+        '<br/>',
+    })
+    await queryInterface.bulkInsert(
+      'SourceOrigin',
+      [
+        {
+          theoretical_material_id: 7,
+          updated_at: '2023-03-19',
+          source_origin: 'https://ihdemu.com/palabras-tritonicas/',
+        },
+      ],
+      {},
+    )
   },
 
   async down(queryInterface, Sequelize) {
