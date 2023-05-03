ea0e4581e6460a031ea336da2179dec3ab8303ad
José Enrique <drilosv13@gmail.com>
2023-05-03 16:36:34 -0600

    feat:Se añaden selectores visuales y en codigo

--- frontend/src/pages/strategies/relationship.vue
+++ frontend/src/pages/strategies/relationship.vue
@@ -8,10 +8,13 @@
 
     <div class="row col-12" v-for="(element,index) in elements" :key="element.uuid" >
 
-      <div class="col-4 col-md-2 q-px-md q-py-md" @click="playAudio($event,require(`../../assets/relationship/${element.audio}`))">
+      <div class="col-4 col-md-2 q-px-md q-py-md">
         <div
           v-ripple
           class="relative-position flex flex-center q-px-md q-pt-md shadow-3 cursor-pointer"
+          :ref="setIMGRef(index)"
+          :style="{ border: selectedIMGIndex === index ? '2px solid red' : '2px solid black' }"
+          @click="playAudio($event,require(`../../assets/relationship/${element.audio}`), index)"
         >
           <q-img
           :src="require(`../../assets/relationship/${element.image}`)"
@@ -29,6 +32,9 @@
           v-ripple
           class="relative-position flex shadow-3 q-px-md q-py-md cursor-pointer"
           style="min-height: 100%; min-width: 100%;"
+          :ref="setRef(index)"
+          :style="{ border: selectedIndex === index ? '2px solid red' : '2px solid black' }"
+          @click="selectItem(index)"
         >
           <div v-html="significados[index].significado"></div>
         </div>
@@ -107,16 +113,71 @@ export default defineComponent({
       significados.value = buffer2.sort(() => Math.random() - 0.5)
     })
 
-    function playAudio (event, path) {
+    function playAudio (event, path, index) {
       const audio = ref(new Audio(path))
       audio.value.currentTime = 0
       audio.value.play()
+      selectIMGItem(index)
+    }
+
+    // SELECTOR Significados
+    const selectedIndex = ref(-1)
+    const selectedRef = ref(null)
+
+    function setRef (index) {
+      return (el) => {
+        if (selectedIndex.value === index) {
+          selectedRef.value = el
+        }
+      }
+    }
+
+    function selectItem (index) {
+      if (selectedIndex.value !== index) {
+        if (selectedRef.value) {
+          selectedRef?.value?.setAttribute('style', 'border: 2px solid black')
+        }
+        selectedIndex.value = index
+        selectedRef.value = document.querySelector(`[ref="item-${index}"]`)
+        selectedRef?.value?.setAttribute('style', 'border: 2px solid red')
+      }
+    }
+
+    // SELECTOR IMAGES
+    const selectedIMGIndex = ref(-1)
+    const selectedIMGRef = ref(null)
+
+    function setIMGRef (index) {
+      return (el) => {
+        if (selectedIndex.value === index) {
+          selectedIMGRef.value = el
+        }
+      }
+    }
+
+    function selectIMGItem (index) {
+      if (selectedIMGIndex.value !== index) {
+        if (selectedIMGRef.value) {
+          selectedIMGRef?.value?.setAttribute('style', 'border: 2px solid black')
+        }
+        selectedIMGIndex.value = index
+        selectedIMGRef.value = document.querySelector(`[ref="item-${index}"]`)
+        selectedIMGRef?.value?.setAttribute('style', 'border: 2px solid red')
+      }
     }
 
     return {
       elements,
       playAudio,
-      significados
+      significados,
+      setRef,
+      selectItem,
+      selectedIndex,
+      selectedRef,
+      selectedIMGIndex,
+      selectedIMGRef,
+      setIMGRef,
+      selectIMGItem
     }
   }
 })
