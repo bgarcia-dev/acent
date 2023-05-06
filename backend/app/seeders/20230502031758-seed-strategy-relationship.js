348ff5a26277ca9f4f01d73843a85381a211f9cb
José Enrique <drilosv13@gmail.com>
2023-05-05 18:13:04 -0600

    feat:Estaregia asociación - Se completa 2/3 del dataset Tritonicas

--- backend/app/seeders/20230502031758-seed-strategy-relationship.js
+++ backend/app/seeders/20230502031758-seed-strategy-relationship.js
@@ -171,6 +171,171 @@ module.exports = {
             },
           ]),
         },
+        {
+          strategy_id: 3,
+          type: 'Tritonica',
+          content: JSON.stringify([
+            {
+              uuid: 'a94afaca-1480-4f31-b321-4bb400523419',
+              word: 'Continuo',
+              acentuacion: 'Grave',
+              image: 'Continuo.jpg',
+              audio: 'Continuo.mp3',
+              significado:
+                '<p style="font-size:16px">Adjetivo o sea que califica a un sustantivo, del tal forma que se puede usar para indicar proximidad o aproximación<br/><br/>Ejemplo: En el edificio <i>proximo<i/> hay varias oficinas de abogados.  <p> <small> <i>Acentuación :  Grave<i> </small>',
+            },
+            {
+              uuid: '99a9dd30-945a-4f50-a880-168f4934dde6',
+              word: 'Continúo',
+              acentuacion: 'Grave',
+              image: 'Continúo.jpg',
+              audio: 'Continúo.mp3',
+              significado:
+                '<p style="font-size:16px">Es la primera persona singular del <b>presente</b> indicativo del verbo continuar, que indica que una acción o proceso sigue sin interrupción.</p> <small> <i>Acentuación :  Grave<i> </small>',
+            },
+            {
+              uuid: '0e5b2554-112e-40ac-a9c0-c382b5d1d714',
+              word: 'Continuó',
+              acentuacion: 'Aguda',
+              image: 'Continuó.png',
+              audio: 'Continuó.mp3',
+              significado:
+                '<p style="font-size:16px">Es la tercera persona singular del pretérito indefinido del verbo continuar, que indica que una acción o proceso siguió sin interrupción en el <b>pasado</b>.</p> <small> <i>Acentuación :  Aguda<i> </small>',
+            },
+          ]),
+        },
+        {
+          strategy_id: 3,
+          type: 'Tritonica',
+          content: JSON.stringify([
+            {
+              uuid: '6bbc8f0a-eb95-11ed-a05b-0242ac120003',
+              word: 'Práctico',
+              acentuacion: 'Esdrújula',
+              image: 'Práctico.jpg',
+              audio: 'Práctico.mp3',
+              significado:
+                '<p style="font-size:16px">Que es útil, conveniente o adecuado para la realización de una tarea o actividad.<p> <small> <i>Acentuación : Esdrújula<i> </small>',
+            },
+            {
+              uuid: '6bbc9374-eb95-11ed-a05b-0242ac120003',
+              word: 'Practico',
+              acentuacion: 'Grave',
+              image: 'Practico.jpg',
+              audio: 'Practico.mp3',
+              significado:
+                '<p style="font-size:16px">Primera persona singular del <b>presente</b> indicativo del verbo practicar, que significa realizar una actividad de manera habitual o para mejorar en ella.<p> <small> <i>Acentuación : Grave<i> </small>',
+            },
+            {
+              uuid: '6bbc975c-eb95-11ed-a05b-0242ac120003',
+              word: 'Practicó',
+              acentuacion: 'Aguda',
+              image: 'Practicó.jpg',
+              audio: 'Practicó.mp3',
+              significado:
+                '<p style="font-size:16px">Tercera persona singular del pretérito indefinido del verbo practicar, que significa realizar una actividad en el <b>pasado</b>.<p> <small> <i>Acentuación : Aguda<i> </small>',
+            },
+          ]),
+        },
+        {
+          strategy_id: 3,
+          type: 'Tritonica',
+          content: JSON.stringify([
+            {
+              uuid: 'da5492e1-8f37-42cc-9980-2d4d0e62879d',
+              word: 'Centrífugo',
+              acentuacion: 'Esdrújula',
+              image: 'Centrífugo.png',
+              audio: 'Centrífugo.mp3',
+              significado:
+                '<p style="font-size:16px">Se refiere a una fuerza o efecto que tiende a alejarse del centro, como la fuerza centrífuga en un objeto que gira.<p> <small> <i>Acentuación : Esdrújula<i> </small>',
+            },
+            {
+              uuid: 'bf49226d-9cec-4bfb-bca6-75a9c9559be5',
+              word: 'Centrifugo',
+              acentuacion: 'Grave',
+              image: 'Centrifugo.jpg',
+              audio: 'Centrifugo.mp3',
+              significado:
+                '<p style="font-size:16px">Es la forma <b>presente</b> del verbo centrifugar, que significa separar o aislar partículas sólidas de un líquido mediante la aplicación de una fuerza centrífuga.<p> <small> <i>Acentuación : Grave<i> </small>',
+            },
+            {
+              uuid: 'd60994e1-572d-4f75-b0c4-b75864d5ea58',
+              word: 'Centrifugó',
+              acentuacion: 'Aguda',
+              image: 'Centrifugó.jpg',
+              audio: 'Centrifugó.mp3',
+              significado:
+                '<p style="font-size:16px">Es la tercera persona del singular del pretérito indefinido del verbo centrifugar, que indica que alguien realizó la acción de centrifugar en el <b>pasado</b>.<p> <small> <i>Acentuación : Aguda<i> </small>',
+            },
+          ]),
+        },
+        {
+          strategy_id: 3,
+          type: 'Tritonica',
+          content: JSON.stringify([
+            {
+              uuid: 'ced298ac-635f-4919-87fb-2d94913a3b51',
+              word: 'Diálogo',
+              acentuacion: 'Esdrújula',
+              image: 'Diálogo.jpg',
+              audio: 'Diálogo.mp3',
+              significado:
+                '<p style="font-size:16px">Conversación entre dos o más personas en la que se intercambian ideas, opiniones o información.<p> <small> <i>Acentuación : Esdrújula<i> </small>',
+            },
+            {
+              uuid: 'e8d9d3ba-306c-4c6c-b1ac-56cc0006065e',
+              word: 'Dialogo',
+              acentuacion: 'Grave',
+              image: 'Dialogo.png',
+              audio: 'Dialogo.mp3',
+              significado:
+                '<p style="font-size:16px">Primera persona del singular del <b>presente</b> de indicativo del verbo dialogar, que significa entablar un diálogo o conversación con alguien.<p> <small> <i>Acentuación : Grave<i> </small>',
+            },
+            {
+              uuid: 'be357531-4872-4da6-8930-75fee2e55d5f',
+              word: 'Dialogó',
+              acentuacion: 'Aguda',
+              image: 'Dialogó.png',
+              audio: 'Dialogó.mp3',
+              significado:
+                '<p style="font-size:16px">Tercera persona del singular del pretérito indefinido del verbo dialogar, que indica que una persona mantuvo una conversación o diálogo con alguien en el <b>pasado</b>.<p> <small> <i>Acentuación : Aguda<i> </small>',
+            },
+          ]),
+        },
+        {
+          strategy_id: 3,
+          type: 'Tritonica',
+          content: JSON.stringify([
+            {
+              uuid: 'e13bac4e-5934-4ca9-a8f7-66bffffeb0a8',
+              word: 'Hábito',
+              acentuacion: 'Esdrújula',
+              image: 'Hábito.jpg',
+              audio: 'Hábito.mp3',
+              significado:
+                '<p style="font-size:16px">Es un sustantivo que se refiere a una costumbre o comportamiento adquirido por la repetición frecuente de ciertas acciones. También puede referirse a la vestimenta o atuendo característico de una orden religiosa.<p> <small> <i>Acentuación : Esdrújula<i> </small>',
+            },
+            {
+              uuid: 'd3e259fd-2c25-44a3-98f1-37bbca8537bb',
+              word: 'Habito',
+              acentuacion: 'Grave',
+              image: 'Habito.jpg',
+              audio: 'Habito.mp3',
+              significado:
+                '<p style="font-size:16px">Es el <b>presente</b> del verbo habitar, que significa vivir en una casa o lugar determinado.<p> <small> <i>Acentuación : Grave<i> </small>',
+            },
+            {
+              uuid: '9a112b27-50fa-47e6-8175-fc05c5073d6c',
+              word: 'Habitó',
+              acentuacion: 'Aguda',
+              image: 'Habitó.jpg',
+              audio: 'Habitó.pm3',
+              significado:
+                '<p style="font-size:16px">Es el pretérito indefinido del verbo habitar, que se utiliza para indicar que alguien vivió en un lugar en el <b>pasado</b>.<p> <small> <i>Acentuación : Aguda<i> </small>',
+            },
+          ]),
+        },
       ],
       {},
     )
