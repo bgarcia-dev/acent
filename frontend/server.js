dbe6b7c404fe7aba310c3bb6a215c091f5875df5
José Enrique <drilosv13@gmail.com>
2023-08-28 23:44:50 -0600

    Se añade libreria para resolver cors

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const history = require('connect-history-api-fallback')
+const cors = require('cors')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors())
 app.use(history())
 // eslint-disable-next-line n/no-path-concat
 app.use(serveStatic(`${__dirname}/dist/spa`))
